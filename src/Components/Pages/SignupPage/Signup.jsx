e4d9d97b2ea6f8e2fcd321b7d860b2631d17215d
Salman-165324 <salmansadi165324@gmail.com>
2023-05-21 05:39:06 +0600

    Did some edits in the signup page's style and designs

--- src/Components/Pages/SignupPage/Signup.jsx
+++ src/Components/Pages/SignupPage/Signup.jsx
@@ -6,31 +6,31 @@ const Signup = () => {
 
     return (
         <div className='primary-container'>
-            <div className="min-h-screen bg-primary rounded-2xl flex flex-col items-center">
-                <h1 className='py-10 text-center text-4xl font-bold accent-text-color'>Welcome, Signup Please!</h1>
-                <div className="hero-content mx-auto flex-col lg:flex-row-reverse lg:gap-10 items-center justify-center">
+            <div className=" bg-primary rounded-2xl flex flex-col items-center">
+                <h1 className='pt-12 pb-4 text-center text-4xl font-bold accent-text-color'>Welcome, Signup Please!</h1>
+                <div className="hero-content mx-auto flex-col xl:flex-row-reverse gap-14 sm:gap-10 items-center justify-center px-4">
                     <div className="text-center lg:text-left">
-                        <Lottie className='h-[500px]' animationData={signupAnimation} />
+                        <Lottie className='sm:h-[500px] w-[400px]' animationData={signupAnimation} />
                     </div>
-                    <div className="card flex-shrink-0 bg-white w-full max-w-sm shadow-2xl mb-10 lg:mb-0 ">
+                    <div className="card flex-shrink-0 bg-neutral1 w-full max-w-sm shadow-2xl mb-10 lg:mb-0 ">
                         <div className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered border-[#272253]" />
+                                <input type="text " placeholder="email" className="input bg-neutral1 input-bordered border-[#272253]" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered border-[#272253]" />
+                                <input type="text" placeholder="password" className="input bg-neutral1 input-bordered border-[#272253]" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary text-white hover:bg-[#f7c12d] hover:border-[#d9b03d] border-[#d9b03d] bg-accent">Login</button>
+                                <button className="btn btn-primary hover:bg-[#a83b39] hover:border-[#d9b03d] border-[#c15352] bg-accent text-neutral1">Login</button>
                             </div>
                         </div>
                     </div>
