d2e6d7fde77b29b13e5da17bbb7d7c7dce11a3b2
Salman-165324 <salmansadi165324@gmail.com>
2023-05-21 05:15:02 +0600

    Changed the color combination for the signup page animation

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = () => {
     const navItems =
         <>
 
-            <Link to="/">Home</Link>
-            <Link to="/all-toys">All Toys</Link>
-            <Link to="/my-toys">My Toys</Link>
-            <Link to="/add-toys">Add A Toy</Link>
-            <Link to="/blog">Blog</Link>
+            <Link className=' hover:text-primary' to="/">Home</Link>
+            <Link className=' hover:text-primary' to="/all-toys">All Toys</Link>
+            <Link className=' hover:text-primary' to="/my-toys">My Toys</Link>
+            <Link className=' hover:text-primary' to="/add-toys">Add A Toy</Link>
+            <Link className=' hover:text-primary' to="/blog">Blog</Link>
             <Link></Link>
 
         </>
@@ -24,14 +24,15 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden text-primary font-extrabold text-2xl mt-1">
                           <IoReorderFourSharp></IoReorderFourSharp>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content top-20 left-4  mt-3 p-4 shadow space-y-4 font-semibold bg-accent text-neutral1 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content top-20 left-4  mt-3 p-4 shadow space-y-4 font-semibold bg-accent text-neutral1  rounded-box w-52">
                             {navItems}
                         </ul>
                     </div>
-                    <Link to="/" className="btn btn-ghost normal-case text-xl accent-text-colo">
+                    <Link to="/" className="btn btn-ghost normal-case text-xl accent-text-colo hover:bg-none">
                         <img className='w-8 h-8 pr-1
                 ' src={logo} alt="Company Logo with a light bulb" />
-                        <span className='text-primary'>Smart</span> <span className='text-neutral1'>Kiddo</span></Link>
+                        <span className='text-primary'>Smart</span> <span className='text-neutral1'>Kiddo</span>
+                        </Link>
                 </div>
                 {/* Large Display nav items */}
                 <div className="navbar-center hidden lg:flex font-semibold">
