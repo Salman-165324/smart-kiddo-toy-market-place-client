fd980771027619e04f9f3d6a77db570c87e0cc5f
Salman-165324 <salmansadi165324@gmail.com>
2023-05-22 05:51:28 +0600

    Added a basic hero section

---
+++ src/Components/Pages/HomePage/Hero.jsx
@@ -0,0 +1,41 @@
+
+
+import heroBg from '../../../assets/heroBg.jpg';
+import heroCharacter from '../../../assets/heroCharacter.png'
+import clouds from '../../../assets/clouds.json'
+import Lottie from "lottie-react";
+
+const Hero = () => {
+    return (
+        // <div className="hero min-h-screen bg-base-200"  style={{ backgroundImage: `url(${heroBg})` }}>
+        //     <div className="hero-content flex-col lg:flex-row-reverse">
+        //         <img src={heroCharacter} className="max-w-sm" />
+        //         <div>
+        //         <Lottie className='sm:h-[500px] w-[400px]' animationData={clouds} />
+        //             <h1 className="text-5xl font-bold text-primary">Develop your kid <br /> with toys!</h1>
+        //             <p className="py-6 text-neutral1">{`Our kids are our future, but it seems like they don't care at all! Don't worry, though. They enjoy playing, right? If we give them the right toys, they will grow just right, even while playing!`}</p>
+        //             <button className="primary-button">Get Started</button>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className='min-h-screen object-cover bg-right	 bg-no-repeat relative' style={{ backgroundImage: `url(${heroBg})` }}>
+            <div className='flex justify-between secondary-container'>
+                <div >
+                    <Lottie className='  sm:h-[600px] w-[500px]' animationData={clouds} />
+                    <div className='ml-14 bottom-[35vh] absolute bg-blend-overlay'>
+                        <h1 className=" text-5xl font-bold text-primary">Develop your kid <br /> with toys!</h1>
+                        <p className="py-6 w-1/2 text-gray-600 font-semibold">{`Our kids are our future, but it seems like they don't care at all! Don't worry, though. They enjoy playing, right? If we give them the right toys, they will grow just right, even while playing!`}</p>
+                        <button className="primary-button">Get Started</button>
+                    </div>
+
+                </div>
+                <div>
+                    <img src={heroCharacter} className=" md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl absolute bottom-0 -right-[14vh] xl:right-[10vh]" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
--- src/Components/Pages/HomePage/Home.jsx
+++ src/Components/Pages/HomePage/Home.jsx
@@ -1,8 +1,9 @@
+import Hero from "./Hero";
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Home</h1>
+            <Hero></Hero>
         </div>
     );
 };
