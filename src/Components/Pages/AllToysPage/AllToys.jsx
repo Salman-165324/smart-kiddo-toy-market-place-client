acdc329cea0546627e435a6fa92a22a939f35bb7
Salman-165324 <salmansadi165324@gmail.com>
2023-05-23 04:47:16 +0600

    Implemented Search functionallity to all the toy page

--- src/Components/Pages/AllToysPage/AllToys.jsx
+++ src/Components/Pages/AllToysPage/AllToys.jsx
@@ -5,6 +5,18 @@ import TableRow from './TableRow';
 const AllToys = () => {
 
     const [products, setProducts] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+
+    const handleSearch = () => {
+
+        fetch(`http://localhost:5000/toysBySearch?inputText=${inputValue}`)
+            .then(res => res.json())
+            .then(data => {
+
+                setProducts(data)
+            })
+    }
 
     useEffect(() => {
 
@@ -19,6 +31,15 @@ const AllToys = () => {
     return (
         <div className='primary-container'>
             <h1 className='section-title'> All the toys</h1>
+            <div className="form-control">
+                <div className="input-group mt-10">
+                    <input type="text" placeholder="Search…" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+                        className="input input-bordered" />
+                    <button onClick={handleSearch} className="btn btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                </div>
+            </div>
             <div className='mt-10'>
                 <div className="overflow-x-auto w-full">
                     <table className="table w-full">
@@ -37,16 +58,16 @@ const AllToys = () => {
                             {
 
                                 products.map(product => <TableRow
-                                
+
                                     key={product._id}
-                                    product = {product}
+                                    product={product}
                                 >
 
                                 </TableRow>)
                             }
-              
+
                         </tbody>
-        
+
 
                     </table>
                 </div>
