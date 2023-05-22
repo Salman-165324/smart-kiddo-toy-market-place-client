b7701d8484ddc12966a0079e4d0a2e2a6d63bed6
Salman-165324 <salmansadi165324@gmail.com>
2023-05-22 23:11:30 +0600

    Implemented Basic Search by Category

--- src/Components/Pages/HomePage/ShoppingCardBlock.jsx
+++ src/Components/Pages/HomePage/ShoppingCardBlock.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ShoppingCardBlock = ({category}) => {
 
-    const [product, setProduct] = useState()
+
+
+    const [product, setProduct] = useState([]); 
+
+    useEffect(() => {
+
+        fetch(`http://localhost:5000/searchByCategory?category=${category}`)
+            .then( res => res.json())
+            .then( data => setProduct(data))
+
+    }, [category])
+
+    
+    console.log(product)
+
     return (
         <div>
             <h1>{category}</h1>
