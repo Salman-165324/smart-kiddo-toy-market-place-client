e2e1c1bc6641078bdb48ce4c24497fe393873708
Salman-165324 <salmansadi165324@gmail.com>
2023-05-22 06:30:04 +0600

    Did some twicking with the hero image

--- src/Components/Pages/HomePage/Hero.jsx
+++ src/Components/Pages/HomePage/Hero.jsx
@@ -21,16 +21,17 @@ const Hero = () => {
 
         <div className='min-h-screen object-cover bg-right	 bg-no-repeat relative' style={{ backgroundImage: `url(${heroBg})` }}>
             <div className='secondary-container'>
-                    <Lottie className='h-[600px] w-[500px] block' animationData={clouds} />
-                    <div className='ml-14 bottom-[35vh] absolute bg-blend-overlay'>
-                        <h1 className=" text-5xl font-bold text-primary">Develop your kid <br /> with toys!</h1>
-                        <p className="py-6 w-1/2 text-gray-600 font-semibold">{`Our kids are our future, but it seems like they don't care at all! Don't worry, though. They enjoy playing, right? If we give them the right toys, they will grow just right, even while playing!`}</p>
+                    {/* <Lottie className='h-[600px] w-[500px] absolute -top-[30vh] -left-16 block' animationData={clouds} />  */}
+                    <div className='ml-14 bottom-[35vh] absolute'>
+                        <h1 className="text-3xl xl:text-5xl font-bold text-primary">Develop your kid <br /> with toys!</h1>
+                        <p className="py-6 w-1/2 text-gray-600">{`Our kids are our future, but it seems like they don't care at all! Don't worry, though. They enjoy playing, right? If we give them the right toys, they will grow just right, even while playing!`}</p>
                         <button className="primary-button">Get Started</button>
                     </div>
-          
+                    
             </div>
+           
             <div>
-                <img src={heroCharacter} className=" md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl absolute bottom-0 -right-[14vh] xl:right-[10vh]" />
+                <img src={heroCharacter} className="overflow-clip max-w-[440px] sm:max-w-lg md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl absolute bottom-0 -right-[22vh] xl:right-[8vh]" />
             </div>
         </div>
     );
